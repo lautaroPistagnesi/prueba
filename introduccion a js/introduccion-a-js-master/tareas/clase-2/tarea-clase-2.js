57a400cab746c0c4d0f773edc76d13f50bcc920d
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2023-03-20 22:40:39 -0300

    sume archivos
    Volví a los videos de js

---
+++ introduccion a js/introduccion-a-js-master/tareas/clase-2/tarea-clase-2.js
@@ -0,0 +1,46 @@
+// TAREA: Tenemos 2 funciones de la tarea anterior -- 'sumar' y 'restar'
+//      Vamos a decirle a la máquina qué función tiene que correr,
+//      dependiendo del operador artmético (+, -, /, *, etc.)
+//
+//      Si el (IF) operador es '+', vamos a usar la función para sumar.
+//      Si no (ELSE), vamos a usar la función para restar.
+//
+// Paso 1 - Crear una variable llamada 'operador' y asignarle el valor '+'.
+// Paso 2 - Crear 2 variables que contengan 2 números cualquiera.
+// Paso 3 - Crear una condición if/else basada en el operador que tenemos.
+//
+// Si (if) tenemos un operador igual a '+', llamamos (ejecutamos. sumar(1,2)) la función
+// 'sumar' con nuestros números (las variables del paso 2).
+// Si no (else), tenemos que llamar la función 'restar', con nuestros números (las variables del paso 2).
+//
+// No se olviden de hacer un console.log para ver el resultado!
+//
+
+/*function sumar (numero1, numero2){;
+    return numero1 + numero2;
+};
+
+function restar (numero1, numero2){;
+    return numero1 - numero2;
+};
+
+let operador = "-";
+const diez = 10;
+const cinco = 5;
+
+if (operador === "+"){;
+    console.log(sumar(diez, cinco));
+}else{;
+    console.log(restar(diez, cinco));
+};*/
+
+
+
+
+// TAREA: Cambiar el operador a '-', y fijarse que llame a la función 'restar' en vez de
+//a la de 'sumar'.
+
+
+
+
+//https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/template_strings
---
+++ introduccion a js/introduccion-a-js-master/tareas/clase-3/tarea-clase-3.js
@@ -0,0 +1,73 @@
+// Tarea 1:
+// Preguntarle al usuario su nombre.
+// Si el nombre del usuario es el mismo que  el  de ustedes
+// Imprimir "Hola, Tocayo! Yo también me llamo " y su nombre.
+// Elijan otro nombre, puede ser de un pariente, amigo, conocido.
+// Si el nombe del usuario es el mismo que el que nombre que eligieron
+// Imprimir "Hola " y el nombre, " te llamás igual que mi ..."
+// Si no, simplemente imprimir "Hola " + nombre!
+
+/*const nombreDelUsuario = prompt("¿Cual es tu nombre?");
+let respuesta;
+if(nombreDelUsuario.toLowerCase() === "lautaro"){;
+    respuesta = 'genial mi nombre tambien es ' + nombreDelUsuario ;
+}else if(nombreDelUsuario.toLowerCase() === "paolo"){;
+    respuesta = "hola " + nombreDelUsuario + ", te llamas igual que mi primo";
+}else{
+    respuesta = "hola " + nombreDelUsuario;
+}
+console.log(respuesta);
+*/
+
+//Tarea 2:
+// Preguntar la edad del usuario
+// Hacerle saber si tiene más, menos ó la misma edad que nosotros.
+
+/*const edadDelUsuario = Number(prompt("¿Cuantos años tenes?"))
+let respuesta;
+if(edadDelUsuario === 15){;
+    respuesta = "genial tenemos la misma edad";
+}else if(edadDelUsuario > 15){;
+    respuesta = "mira vos che sos mayor que yo";
+}else if(edadDelUsuario < 15){;
+    respuesta = "ahhh asi que sos mas chico que yo";
+}else{;
+    respuesta = "eso ni es un numero crack"
+};
+console.log(respuesta);
+*/
+
+//Tarea 3:
+// Preguntarle al usuario si tiene documento, y que conteste con "si" o "no".
+// Si dice si, preguntarle la edad.
+// Si la edad es mayor a 18, dejarlo entrar al bar.
+// Si la edad es menor a 18, no dejarlo entrar al bar.
+// Si no tiene documento, no dejarlo entrar al bar.
+// Si no entendemos la respuesta, le decimos que no entendimos la respuesta.
+// Punto bonus: SI, NO, Si, No, si, no.
+
+/*const documentoDelUsuario = prompt("¿tiene docuemnto?");
+let respuesta;
+if(documentoDelUsuario.toLowerCase() === "si"){;
+    const edadDelUsuario = Number(prompt("¿cuantos años tenes?"));
+    if(edadDelUsuario >= 18){;
+        respuesta = "Puede pasar el bar";
+    }else if(edadDelUsuario < 18){;
+        respuesta = "No puede pasar al bar";
+    }else{;
+        respuesta = "Basta de chistes, decime cuantos años tenes ";
+    };
+}else if(documentoDelUsuario.toLowerCase() === "no"){;
+    respuesta = "Disculpe no puede pasar al bar";;
+}else {;
+    respuesta = "Tenes o no documento";
+};
+console.log(respuesta);
+*/
+
+
+
+
+
+
+
