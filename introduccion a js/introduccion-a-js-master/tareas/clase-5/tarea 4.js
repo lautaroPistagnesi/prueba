d59d1f33a81216af5259876869f076e0da31b3c6
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2023-04-01 15:07:42 -0300

    ultima  tarea clase 3
    estoy probando formas de hacer la ultima tarea de la clase 3

---
+++ introduccion a js/introduccion-a-js-master/tareas/clase-5/tarea 4.js
@@ -0,0 +1,12 @@
+const numerosArray = document.querySelectorAll("li")
+console.log(numerosArray)
+let i;
+let numeroTotal = 0
+for(i = 0; i < numerosArray.length; i += 1){
+    numeroTotal + numerosArray[i]
+    console.log(numeroTotal)
+}
+
+function promedio (uno){
+  
+}
