aee7440f63efa1dfcdcd0397deebf62ec37a4afe
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2023-04-03 22:04:19 -0300

    tarea final de clase 5
    tengo que pensar como sacar el promedio de los numeros

--- introduccion a js/introduccion-a-js-master/tareas/clase-5/tarea 4.js
+++ introduccion a js/introduccion-a-js-master/tareas/clase-5/tarea 4.js
@@ -1,12 +1,13 @@
+const funcionPasarNumero = function pasarANumeros(){
+    let i;
+    for(i=0; i < numerosArray.length; i += 1){
+        console.log(numerosArray[i].textContent)
+       }  
+}
+function sumar(uno, dos){
+    return uno/dos
+}
+console.log(sumar(10, 5))
 const numerosArray = document.querySelectorAll("li")
-console.log(numerosArray)
-let i;
-let numeroTotal = 0
-for(i = 0; i < numerosArray.length; i += 1){
-    numeroTotal + numerosArray[i]
-    console.log(numeroTotal)
-}
-
-function promedio (uno){
-  
-}
+console.log(funcionPasarNumero())
+let mensaje = document.querySelector("mensaje")
