4a0a9d44ccf99e0954416f39cdfede88b6d72923
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2023-04-05 13:39:01 -0300

    tarea 3 clase 5
    termine la tarea, aunque se puede mejorar

--- introduccion a js/introduccion-a-js-master/tareas/clase-5/tarea 4.js
+++ introduccion a js/introduccion-a-js-master/tareas/clase-5/tarea 4.js
@@ -20,7 +20,35 @@ let numero4 = Number(numerosArray[4].textContent)
 function sumar(numero0, numero1, numero2, numero3, numero4){
    return numero0 + numero1 + numero2 + numero3 + numero4
 }
-
-console.log(sumar(numero0, numero1, numero2, numero3, numero4))
 let mensaje = document.querySelector("#mensaje")
 mensaje.textContent = "El promedio es de: " + sumar(numero0, numero1, numero2, numero3, numero4)
+let mensaje2 = document.querySelector("#mensaje2")
+mensaje2.textContent = "El numero mas chico es: " + numero2
+let mensaje3 = document.querySelector("#mensaje3")
+mensaje3.textContent = "El numero mas grande es:" + numero3
+let mensaje4 = document.querySelector("#mensaje4")
+mensaje4.textContent = "El numero que mas se repite es el : " + 3
+
+/*function numeroMasChico(numero0, numero1, numero2, numero3, numero4){
+    let numeroChico = 0
+    if(numero0 < numero1){
+        if(numero0 < numero2){
+            if(numero0 < numero3){
+                if (numero0 < numero3){
+                    numeroChico = numero0
+                }
+            }
+        }else{
+            numeroChico = numero2
+        }
+    }else if(numero1 < numero2){
+            if(numero1 < numero3){
+                if(numero1 < numero4){
+                    numeroChico = numero1
+                }
+        }
+    }else if(numero2 < 43){
+
+    }
+    console.log(numeroChico)
+}*/
