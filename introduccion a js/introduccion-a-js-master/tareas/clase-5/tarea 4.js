ecea2b6b1831d9807f684131c141f7eef4878f1d
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2023-04-04 23:10:23 -0300

    tarea 3 de clase 5
    casi la termino

--- introduccion a js/introduccion-a-js-master/tareas/clase-5/tarea 4.js
+++ introduccion a js/introduccion-a-js-master/tareas/clase-5/tarea 4.js
@@ -1,13 +1,26 @@
+const numerosArray = document.querySelectorAll("li")
 const funcionPasarNumero = function pasarANumeros(){
     let i;
-    for(i=0; i < numerosArray.length; i += 1){
+    for(i = 0; i < numerosArray.length; i += 1){
+        if(i < 5){
         console.log(numerosArray[i].textContent)
-       }  
+        }else{
+            
+        }
+    } 
 }
-function sumar(uno, dos){
-    return uno/dos
-}
-console.log(sumar(10, 5))
-const numerosArray = document.querySelectorAll("li")
 console.log(funcionPasarNumero())
-let mensaje = document.querySelector("mensaje")
+
+let numero0 = Number(numerosArray[0].textContent)
+let numero1 = Number(numerosArray[1].textContent)
+let numero2 = Number(numerosArray[2].textContent)
+let numero3 = Number(numerosArray[3].textContent)
+let numero4 = Number(numerosArray[4].textContent)
+
+function sumar(numero0, numero1, numero2, numero3, numero4){
+   return numero0 + numero1 + numero2 + numero3 + numero4
+}
+
+console.log(sumar(numero0, numero1, numero2, numero3, numero4))
+let mensaje = document.querySelector("#mensaje")
+mensaje.textContent = "El promedio es de: " + sumar(numero0, numero1, numero2, numero3, numero4)
