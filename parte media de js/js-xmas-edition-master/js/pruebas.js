c55e43ce85591ae9afd4341b0c0e8d9356ae15ae
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2023-05-05 16:23:04 -0300

    tarea clase 7
    Termine la tarea de la clase 7 (creo).

--- parte media de js/js-xmas-edition-master/js/pruebas.js
+++ parte media de js/js-xmas-edition-master/js/pruebas.js
@@ -3,13 +3,27 @@ function probarValidarNombre() {
       validarNombre('') === 'Este campo debe tener al menos 1 caracter',
       'Validar nombre no validó que el nombre no sea vacío',
   );
-}
- /* console.assert(
+ console.assert(
       validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
+      '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
       'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+      'Validar nombre no validó que el nombre sea menor a 50 caracteres'
   );
-*/
+}
+probarValidarNombre()
 
-probarValidarNombre();
+function validarCiudadUsuario(){
+  console.assert(
+      validarCiudad("") === "La ciudad no puede ser un espacio vacio", 
+      "validarCiudad no valido que el usuario seleccionara una ciudad"
+  );
+}
+validarCiudadUsuario()
+
+function pruebaValidarRegalo(){
+  console.assert(
+    validarRegalo("") === "el regalo no puede ser vacio",
+    "validarRegalo no valido que el regalo fuera vacio"
+  )
+}
+pruebaValidarRegalo()
