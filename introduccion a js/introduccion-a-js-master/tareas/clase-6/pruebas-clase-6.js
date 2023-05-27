31ddca89d44fd7c0c4a1ee3dc948345463f18c06
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2023-05-26 21:07:39 -0300

    tarea clase 6 punto 2
    Termine con el punto 2 de la tarea 6

--- introduccion a js/introduccion-a-js-master/tareas/clase-6/pruebas-clase-6.js
+++ introduccion a js/introduccion-a-js-master/tareas/clase-6/pruebas-clase-6.js
@@ -1,6 +1,6 @@
 function probarValidarCantidadFamilia(){
     console.assert(
-        validarCantidaFamilia("") === "los integrantes de tu familia no pueden ser 0",
+        validarCantidaFamilia("") === "los integrantes de tu familia no pueden ser 0 o nada",
         "ValidarCantidadFamilia no valido que la cantidad de integrantes de la familia no fuera 0"
     )
     console.assert(
@@ -8,7 +8,9 @@ function probarValidarCantidadFamilia(){
         "validarCantidaFamilia no valido que los integrantes de la familia fueran numeros"
     )
 }
-
+function provarValidarErrorEdad(){
+console.assert(
+    validarCantidaFamilia("") === "la edad de tu familiar no puede ser nada o 0",
+    "ValidarCantidadFamilia no valido que la cantidad de integrantes de la familia no fuera 0"
+)}
 probarValidarCantidadFamilia()
-
-
--- introduccion a js/introduccion-a-js-master/tareas/clase-6/tarea clase 6.js
+++ introduccion a js/introduccion-a-js-master/tareas/clase-6/tarea clase 6.js
@@ -1,32 +1,45 @@
-let IntegrantesFamilia = Number(document.querySelector("#numeroIntegrantesFamilia").value)
-
+let integrantesFamilia = Number(document.querySelector("#numeroIntegrantesFamilia").value)
+let contadorBoton = 0
+let botonApretado = 0
 function validarCantidaFamilia(IntegrantesFamilia){
+
     if(IntegrantesFamilia.length === 0){
-        return "los integrantes de tu familia no pueden ser 0"
+        return "los integrantes de tu familia no pueden ser 0 o nada"
     }
     if(/^[0-9]+$/.test(IntegrantesFamilia)){
         return "los integrantes de tu familia no pueden ser letras"
     }
     return ""
 }
-validarCantidaFamilia(IntegrantesFamilia)
-let contadorBoton = 0
+
+function validarErrorEdad(edadFamiliar){
+    if(edadFamiliar === ""){
+        return "la edad de tu familiar no puede ser nada o 0"
+    }
+    return ""
+}
+
+
+validarCantidaFamilia(integrantesFamilia)
 const botonEnviar = document.querySelector("#botonEnviar")
 botonEnviar.onclick = function(){
-    let IntegrantesFamilia = Number(document.querySelector("#numeroIntegrantesFamilia").value)
+    let integrantesFamilia = Number(document.querySelector("#numeroIntegrantesFamilia").value)
     let i;
     contadorBoton++
-    if(IntegrantesFamilia === 0){
+
+    if(integrantesFamilia === 0){
         document.querySelector("#numeroIntegrantesFamilia").className = "error"
         let lugarErrores = document.querySelector("ul")
         let $error = document.createElement("ul")
-        let error = document.createTextNode("los integrantes de tu familia no pueden ser nada")
+        let error = document.createTextNode("los integrantes de tu familia no pueden ser nada o 0")
         $error.appendChild(error)
         $error.className = "errores"
+
             if(contadorBoton === 1){
                 lugarErrores.appendChild($error)
             }else{
                 let errorImpreso = document.querySelector(".errores")
+
                 if(errorImpreso === null){
                     lugarErrores.appendChild($error)
                 }else{
@@ -34,8 +47,30 @@ botonEnviar.onclick = function(){
                     lugarErrores.appendChild($error)
                 }
             }
+    }else if(!/^[0-9]+$/.test(integrantesFamilia)){
+        document.querySelector("#numeroIntegrantesFamilia").className = "error"
+        let lugarErrores = document.querySelector("ul")
+        let $error = document.createElement("ul")
+        let error = document.createTextNode("los integrantes de tu familia no pueden ser letras")
+        $error.appendChild(error)
+        $error.className = "errores"
+
+        if(contadorBoton === 1){
+            lugarErrores.appendChild($error)
+        }else{
+            let errorImpreso = document.querySelector(".errores")
+
+            if(errorImpreso === null){
+                lugarErrores.appendChild($error)
+            }else{
+                errorImpreso.remove()
+                lugarErrores.appendChild($error)
+            }
+        }
+
     }else{
         let errorImpreso = document.querySelector(".errores")
+
         if(errorImpreso === null){
             
         }else{
@@ -44,8 +79,9 @@ botonEnviar.onclick = function(){
         document.querySelector("div").className = "oculto"
         document.querySelector("#calcular").className = ""
         let inputError = "errorInput"
-        let parrafoError = "errorParrafo"
-        for(i = 0; i < IntegrantesFamilia; i++){
+        let claseParrafo = "parrafo"
+
+        for(i = 0; i < integrantesFamilia; i++){
             document.querySelector("#numeroIntegrantesFamilia").className = ""
             let $form = document.querySelector("form")
             let label = document.createElement("label")
@@ -53,9 +89,9 @@ botonEnviar.onclick = function(){
             let mensaje = document.createTextNode("Ingresa la edad de uno de tu familia")
             let parrafo = document.createElement("p")
             inputError += "I"
-            input.className = inputError
-            parrafoError += "I"
-            parrafo.className = parrafoError
+            claseParrafo += "I"
+            input.name = inputError
+            parrafo.className = claseParrafo
             label.appendChild(mensaje)
             parrafo.appendChild(label)
             parrafo.appendChild(input)
@@ -64,19 +100,126 @@ botonEnviar.onclick = function(){
     }
     return false
 }
+
+function promedio(numero){
+    const integrantesFamilia = Number(document.querySelector("#numeroIntegrantesFamilia").value)
+    let promedioEdad = numero / integrantesFamilia
+    return promedioEdad
+}
+
+function numeroMaximo(arrayNumeros){
+    const integrantesFamilia = Number(document.querySelector("#numeroIntegrantesFamilia").value)
+    let segundoArrayNumeros = []
+    let i;
+
+    for(i = 0; i < integrantesFamilia; i++){
+        segundoArrayNumeros.push(Number(arrayNumeros[i]))
+    }
+    return Math.max(...segundoArrayNumeros)
+}
+
+function numeroMinimo(arrayNumeros){
+    const integrantesFamilia = Number(document.querySelector("#numeroIntegrantesFamilia").value)
+    let segundoArrayNumeros = []
+    let i;
+
+    for(i = 0; i < integrantesFamilia; i++){
+        segundoArrayNumeros.push(Number(arrayNumeros[i]))
+    }
+    return Math.min(...segundoArrayNumeros)
+}
+
 const botonCalcular = document.querySelector("#calcular")
 botonCalcular.onclick = function(){
+
+    botonApretado++
+    let errores = 0
+    let arrayFamiliares = []
     let inputError = "errorInput"
-    let parrafoError = "errorParrafo"
+    let errorInput = "errorInput"
+    let parrafoClase = "parrafo"
+    let claseLabel = "label"
     const integrantesFamilia = Number(document.querySelector("#numeroIntegrantesFamilia").value)
+    const $form = document.querySelector("form")
     let i;
     let sumaEdades = 0
+
     for(i = 0; i < integrantesFamilia; i++){
         inputError += "I"
-        let input = Number(document.querySelector("." + inputError).value)
-        sumaEdades += input
-        console.log(input)
+        let input = $form[inputError].value
+        arrayFamiliares.push(input)
     }
-    console.log(sumaEdades)
 
+    arrayFamiliares.forEach(function(edadFamiliares){
+        errorInput += "I"
+        parrafoClase += "I"
+        claseLabel += "I"
+
+        if(edadFamiliares === ""){
+            errores++
+            let mensajeError = "la edad de tu familiar no puede ser nada o 0"
+            $form[errorInput].className = "error"
+            validarErrorEdad(edadFamiliares)
+            const label = document.createElement("label")
+            label.className = claseLabel
+            label.innerText = mensajeError
+            let parrafoError = document.querySelector("." + parrafoClase)
+
+            if(botonApretado === 1){
+                parrafoError.appendChild(label)
+            }else{
+                let labelClase = document.querySelector("." + claseLabel)
+
+                if(labelClase === null){
+                    parrafoError.appendChild(label)
+                }else{
+                    labelClase.remove()
+                    parrafoError.appendChild(label)
+                }
+            }
+        }else if(!/^[0-9]+$/.test(edadFamiliares)){
+            errores++
+            let mensajeError = "la edad de tu familiar no puede ser letras o espacios"
+            $form[errorInput].className = "error"
+            validarErrorEdad(edadFamiliares)
+            const label = document.createElement("label")
+            label.className = claseLabel
+            label.innerText = mensajeError
+            let parrafoError = document.querySelector("." + parrafoClase)
+
+            if(botonApretado === 1){
+                parrafoError.appendChild(label)
+            }else{
+                let labelClase = document.querySelector("." + claseLabel)
+
+                if(labelClase === null){
+                    parrafoError.appendChild(label)
+                }else{
+                    labelClase.remove()
+                    parrafoError.appendChild(label)
+                }
+            }
+
+        }else{
+            $form[errorInput].className = ""
+            sumaEdades += Number(edadFamiliares)
+            let labelClaseII = document.querySelector("." + claseLabel)
+            if(labelClaseII != null){
+                labelClaseII.remove()
+            }
+        }
+    })
+
+    if(errores === 0){
+        document.querySelector("form").className = "oculto"
+        document.querySelector("#calcular").className = "oculto"
+        document.querySelector("#resultado").className = ""
+        document.querySelector("#promedioEdad").value = promedio(sumaEdades)
+        document.querySelector("#mayorEdad").value = numeroMaximo(arrayFamiliares)
+        document.querySelector("#menorEdad").value = numeroMinimo(arrayFamiliares)
+    }
+}
+
+let botonReset = document.querySelector("#botonReset")
+botonReset.onclick = function(){
 }
