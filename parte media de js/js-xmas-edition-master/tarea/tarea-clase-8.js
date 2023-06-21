b615f8fb6abed7e9bbf5adfa963c52d1a897bcfa
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2023-06-21 20:55:59 -0300

    no se que son estos archivos

---
+++ parte media de js/js-xmas-edition-master/tarea/tarea-clase-8.js
@@ -0,0 +1,8 @@
+/*# Tarea clase 8
+
+A las 2 tareas de la clase 6, ponerles las validaciones que consideren
+necesarias.(usando RegExp, Objetos, forEach, poner estilos, mostrar los errores en la interfaz del usuario,
+    y escribir pruebas)
+
+TIP: Las edades no pueden tener decimales.
+*/
