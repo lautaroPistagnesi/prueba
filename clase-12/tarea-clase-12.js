3fde3cad7312586db40d7043d2ee80abe43583a6
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2023-08-26 00:25:07 -0300

    Insertar imagenes
    Empeze a insertar imagenes en la pokedex para que se vea el pokemon, queda poco.

--- clase-12/tarea-clase-12.js
+++ clase-12/tarea-clase-12.js
@@ -2,14 +2,18 @@
 
 const botonBuscar = document.querySelector("#buscar")
 botonBuscar.onclick = () => {
-    let numeroPokemones = document.querySelector("#numeroPokemon").value
-    console.log(numeroPokemones)
+    let numeroPokemones = document.querySelector("#numeroPokemon")
+    if(Number(numeroPokemones.value) === Number()){
+        console.log("numero")
+    }else if(!/^[A-z]+$/.test(numeroPokemones)){
+        console.log($(numeroPokemones)[0].value)
+    }
 }
 
 
 fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur")
     .then(respuesta => respuesta.json())
     .then(respuestaJSON => {
-       
+       console.log(respuestaJSON.sprites.front_default)
     })
 
