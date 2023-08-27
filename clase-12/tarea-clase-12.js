21836b73323135fe0020c28edaac811fabff891c
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2023-08-27 04:10:43 -0300

    Fotos de los pokémones
    Hoy logre hacer que las fotos cambiaran al elegir al Pokémon, en su forma normal y en la forma shiny.

--- clase-12/tarea-clase-12.js
+++ clase-12/tarea-clase-12.js
@@ -2,18 +2,32 @@
 
 const botonBuscar = document.querySelector("#buscar")
 botonBuscar.onclick = () => {
-    let numeroPokemones = document.querySelector("#numeroPokemon")
-    if(Number(numeroPokemones.value) === Number()){
-        console.log("numero")
-    }else if(!/^[A-z]+$/.test(numeroPokemones)){
-        console.log($(numeroPokemones)[0].value)
+    let numeroPokemones = document.querySelector("#numeroPokemon").value
+    if(Number(numeroPokemones) === 0){
+        console.log("No puede ser 0 o nada")
+    }else if(Number(numeroPokemones) <= 1010){
+            fetch("https://pokeapi.co/api/v2/pokemon/" + numeroPokemones)
+                .then(respuesta => respuesta.json())
+                .then(respuestaJSON => {
+                    console.log(respuestaJSON.sprites)
+                    $("#pokemonNormal")[0].src = respuestaJSON.sprites.front_default
+                    $("#pokemonShiny")[0].src = respuestaJSON.sprites.front_shiny
+                })
+    }else if(/^[A-z]+$/.test(numeroPokemones)){
+        fetch("https://pokeapi.co/api/v2/pokemon/" + numeroPokemones)
+            .then(respuesta => respuesta.json())
+            .then(respuestaJSON => {
+                $("#pokemonNormal")[0].src = respuestaJSON.sprites.front_default
+                $("#pokemonShiny")[0].src = respuestaJSON.sprites.front_shiny
+            })
+    }else if(Number(numeroPokemones) >= 1011){
+        console.log("solo exiten 1010 pokemones")
+    }else{
+        console.log("no podes poner letras con numero o simbolos")
     }
 }
 
 
-fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur")
-    .then(respuesta => respuesta.json())
-    .then(respuestaJSON => {
-       console.log(respuestaJSON.sprites.front_default)
-    })
+
+
 
