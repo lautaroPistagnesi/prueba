5aa5dedf21705c2878724228b7f4fd5018237db6
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2023-08-31 00:35:54 -0300

    mejora de cofdigo
    Lo unico que hice fue mejorar el codigo, ahora no se repite el mensaje de error y lo simplifique usando una funcion.

--- clase-12/tarea-clase-12.js
+++ clase-12/tarea-clase-12.js
@@ -1,33 +1,58 @@
 /// <reference types="jquery" />
+let nombreValido = ""
+let CambiarImagenes = function(numeroPokemones){
+    fetch("https://pokeapi.co/api/v2/pokemon/" + numeroPokemones)
+    .then(respuesta => {if(respuesta.ok === false){
+                           nombreValido = "no"
+                        }}
+    )
+    .then(respuesta => respuesta.json())
+    .then(respuestaJSON => {
+        $("#pokemonNormal")[0].src = respuestaJSON.sprites.front_default
+        $("#pokemonShiny")[0].src = respuestaJSON.sprites.front_shiny
+    })
+}
 
-const botonBuscar = document.querySelector("#buscar")
-botonBuscar.onclick = () => {
-    let numeroPokemones = document.querySelector("#numeroPokemon").value
-    if(Number(numeroPokemones) === 0){
-        console.log("No puede ser 0 o nada")
-    }else if(Number(numeroPokemones) <= 1010){
-            fetch("https://pokeapi.co/api/v2/pokemon/" + numeroPokemones)
-                .then(respuesta => respuesta.json())
-                .then(respuestaJSON => {
-                    console.log(respuestaJSON.sprites)
-                    $("#pokemonNormal")[0].src = respuestaJSON.sprites.front_default
-                    $("#pokemonShiny")[0].src = respuestaJSON.sprites.front_shiny
-                })
-    }else if(/^[A-z]+$/.test(numeroPokemones)){
-        fetch("https://pokeapi.co/api/v2/pokemon/" + numeroPokemones)
-            .then(respuesta => respuesta.json())
-            .then(respuestaJSON => {
-                $("#pokemonNormal")[0].src = respuestaJSON.sprites.front_default
-                $("#pokemonShiny")[0].src = respuestaJSON.sprites.front_shiny
-            })
-    }else if(Number(numeroPokemones) >= 1011){
-        console.log("solo exiten 1010 pokemones")
+const agregarError = function(mensaje){
+    const lugarDestino = document.querySelector("#lugarBusqueda")
+    $("#numeroPokemon")[0].className = "border border-danger"
+    $("#buscar")[0].className = "border border-danger"
+    let $label = document.createElement("label")
+    $label.className = "text-danger fs-4"
+    $label.id = "mensajeError"
+    const texto = document.createTextNode(mensaje)
+    $label.appendChild(texto)
+    if($("#mensajeError").length === 0){
+        lugarDestino.append($label)
     }else{
-        console.log("no podes poner letras con numero o simbolos")
+        $("#mensajeError").remove()
+        lugarDestino.append($label)
+    }
+}
+const sacarError = function(){
+    $("#numeroPokemon")[0].className = ""
+    $("#buscar")[0].className = ""
+    if($("#mensajeError").length != 0){
+       $("#mensajeError").remove()
     }
 }
 
-
-
-
-
+const botonBuscar = document.querySelector("#buscar")
+botonBuscar.onclick = () => {
+    $("#numeroPokemon")[0].className = ""
+    $("#buscar")[0].className = ""
+    let numeroPokemones = document.querySelector("#numeroPokemon").value
+    if(Number(numeroPokemones) === 0){
+        agregarError("No puede ser 0 o nada")
+    }else if(Number(numeroPokemones) <= 1010){
+        sacarError()
+        CambiarImagenes(numeroPokemones)
+    }else if(/^[A-z]+$/.test(numeroPokemones)){
+        sacarError()
+        CambiarImagenes(numeroPokemones)
+    }else if(Number(numeroPokemones) >= 1011){
+        agregarError("solo hay 1010 pokemones")
+    }else{
+        agregarError("no podes poner letras con numero o simbolos")
+    }
+}
